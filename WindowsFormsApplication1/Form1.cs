e1def6e4fc0b77b869a7f35833173de81a1d11e3
GitVictorChen <victor80@163.com>
2017-10-27 16:06:23 +0800

    Change MessageBox

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("First Test App");
+            MessageBox.Show("First Test App 2");
         }
     }
 }
